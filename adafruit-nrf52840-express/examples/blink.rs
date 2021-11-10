71c8a7848995a5e933d4a64b4d89afcaf77cc10d
Natesh Narain <nnaraindev@gmail.com>
2021-11-09 19:38:15 -0500

    Resolves #7. Pins struct

--- adafruit-nrf52840-express/examples/blink.rs
+++ adafruit-nrf52840-express/examples/blink.rs
@@ -1,16 +1,45 @@
+//
+// blink.rs
+//
+// @author Natesh Narain <nnaraindev@gmail.com>
+// @date Nov 09 2021
+//
 #![no_std]
 #![no_main]
 
 use panic_halt as _;
-use cortex_m::asm;
 
 use adafruit_nrf52840_express as bsp;
 
-#[cortex_m_rt::entry]
+use bsp::{
+    entry,
+    hal::{self, prelude::*},
+    Pins,
+};
+
+#[entry]
 fn main() -> ! {
+    let cp = bsp::core::Peripherals::take().unwrap();
     let dp = bsp::hal::pac::Peripherals::take().unwrap();
 
+    let mut delay = hal::Delay::new(cp.SYST);
+
+    let pins = Pins::new(dp.P0, dp.P1);
+
+    let mut led1 = pins.led;
+    let mut led2 = pins.blue_led;
+
+    led2.set_high().unwrap();
+
     loop {
-        asm::nop();
+        led1.set_high().unwrap();
+        led2.set_low().unwrap();
+
+        delay.delay_ms(500u16);
+
+        led1.set_low().unwrap();
+        led2.set_high().unwrap();
+
+        delay.delay_ms(500u16);
     }
 }
