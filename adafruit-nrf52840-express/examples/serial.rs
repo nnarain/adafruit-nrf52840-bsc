192ba24ec063ac8c811bf93aaf10aebe19648ccd
Natesh Narain <nnaraindev@gmail.com>
2021-11-09 22:34:38 -0500

    Common serial init

--- adafruit-nrf52840-express/examples/serial.rs
+++ adafruit-nrf52840-express/examples/serial.rs
@@ -13,7 +13,11 @@ use adafruit_nrf52840_express as bsp;
 
 use bsp::{
     entry,
-    hal::{self, prelude::*, gpio, uarte},
+    prelude::*,
+    hal::{
+        Delay,
+        uarte::{Parity, Baudrate}
+    },
     Pins,
 };
 use core::fmt::Write;
@@ -23,31 +27,21 @@ fn main() -> ! {
     let cp = bsp::core::Peripherals::take().unwrap();
     let dp = bsp::hal::pac::Peripherals::take().unwrap();
 
-    let mut delay = hal::Delay::new(cp.SYST);
+    let mut delay = Delay::new(cp.SYST);
 
     let pins = Pins::new(dp.P0, dp.P1);
 
-    let (rx, tx) = (
-        pins.rx.into_floating_input().degrade(),
-        pins.tx.into_push_pull_output(gpio::Level::High).degrade()
-    );
-
-    let uart_pins = uarte::Pins {
-        rxd: rx,
-        txd: tx,
-        cts: None,
-        rts: None,
-    };
-
-    let mut uart = uarte::Uarte::new(
+    let mut uart = serial::init(
         dp.UARTE0,
-        uart_pins,
-        uarte::Parity::EXCLUDED,
-        uarte::Baudrate::BAUD115200
+        pins.rx,
+        pins.tx,
+        Baudrate::BAUD115200,
+        Parity::EXCLUDED
     );
 
     loop {
         write!(uart, "Hello World!\r\n").unwrap();
+
         delay.delay_ms(1000u16);
     }
 }
