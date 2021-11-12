fb42c7c739d6a030c99b36b74ebaa512f3a3609b
Natesh Narain <nnaraindev@gmail.com>
2021-11-12 18:16:46 -0500

    Resolves #2. Magnetometer

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 
 Board support crates for Adafruit's nRF52840 based boards.
 
+| Board   | Info Page                                                            |
+| ------- | -------------------------------------------------------------------- |
+| Sense   | https://learn.adafruit.com/adafruit-feather-sense                    |
+| Express | https://learn.adafruit.com/introducing-the-adafruit-nrf52840-feather |
+
 **Examples**
 
 Install cargo-just
--- adafruit-nrf52840-sense/examples/magnetometer.rs
+++ adafruit-nrf52840-sense/examples/magnetometer.rs
@@ -7,9 +7,6 @@
 #![no_std]
 #![no_main]
 
-use nb::block;
-use panic_halt as _;
-
 use adafruit_nrf52840_sense as bsp;
 
 use bsp::{
@@ -21,7 +18,7 @@ use bsp::{
         uarte::{Baudrate, Parity},
     },
     Pins,
-    sensors::lis3mdl::Lis3mdl,
+    sensors::lis3mdl::{Lis3mdl, Address},
 };
 use core::fmt::Write;
 
@@ -49,7 +46,7 @@ fn main() -> ! {
     };
     let i2c = Twim::new(dp.TWIM0, twim_pins, twim::Frequency::K400);
 
-    let mut magnetometer = Lis3mdl::new(i2c);
+    let magnetometer = Lis3mdl::new(i2c, Address::Addr1C);
 
     if let Err(e) = magnetometer {
         write!(uart, "Error: {:?}", e).unwrap();
@@ -67,6 +64,13 @@ fn main() -> ! {
 
         let data = magnetometer.get_mag_axes_mgauss().unwrap();
 
-        write!(uart, "{:?}", data).unwrap();
+        write!(uart, "x: {}, y: {}, z: {}\r\n", data.x, data.y, data.z).unwrap();
+    }
+}
+
+#[panic_handler]
+fn panic(_: &core::panic::PanicInfo) -> ! {
+    loop {
+        cortex_m::asm::bkpt();
     }
 }
